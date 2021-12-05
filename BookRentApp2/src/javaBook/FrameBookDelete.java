897360680f94aea9d92b4f9f375929415d2e2e9c
hiwhwnsgh <hiwhwnsgh@gmail.com>
2021-12-05 19:15:42 +0900

    Book_frame 구현

--- BookRentApp2/src/javaBook/FrameBookDelete.java
+++ BookRentApp2/src/javaBook/FrameBookDelete.java
@@ -1,26 +1,75 @@
 package javaBook;
 
-import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
-// [도서 삭제] 프레임 클래스 
-public class FrameBookDelete extends JFrame {
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import javax.swing.UIManager;
+import javax.swing.JButton;
+
+public class DELETEBOOK {
+
+	private JFrame frame;
 
 	/**
-	 * Create the frame.
+	 * Launch the application.
 	 */
-	public FrameBookDelete() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DELETEBOOK window = new DELETEBOOK();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	/**
+	 * Create the application.
+	 */
+	public DELETEBOOK() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 555, 689);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		BOOKJPanel book_panel = new BOOKJPanel(frame);
+		frame.getContentPane().add(book_panel, BorderLayout.CENTER);
+		
+		JButton DELETE_BUTTON = new JButton("\uC0AD\uC81C");
+		DELETE_BUTTON.setFont(new Font("맑은 고딕", Font.PLAIN, 17));
+		DELETE_BUTTON.setBounds(342, 574, 80, 28);
+		book_panel.add(DELETE_BUTTON);
+		DELETE_BUTTON.addActionListener(new  ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showConfirmDialog(null,"도서를 삭제 하시겠습니까?","도서 삭제",JOptionPane.ERROR_MESSAGE);
+			}
+		});
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(UIManager.getColor("CheckBoxMenuItem.selectionBackground"));
+		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
+		
+		JLabel lblNewLabel = new JLabel("\uB3C4\uC11C \uC0AD\uC81C");
+		lblNewLabel.setForeground(Color.WHITE);
+		lblNewLabel.setFont(new Font("맑은 고딕", Font.BOLD, 17));
+		panel_1.add(lblNewLabel);
+	}
 }
